bcc6c85e2c13446189af539171563fb457080e6b
Asapanna Rakesh <rakeshark22@gmail.com>
2020-10-22 23:03:05 +0530

    todo: add tf, preprocess

--- main.py
+++ main.py
@@ -35,19 +35,31 @@ import cv2
 import numpy as np
 import base64
 import matplotlib.pyplot as plt
+#from tf.keras.applications.efficientnet import preprocess_input
 
 def readb64(uri):
    encoded_data = uri.split(',')[1]
    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-   print(nparr.shape)
+   print('BEF:',nparr.shape)
    img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-   print(img.shape, type(img))
-   cv2.imwrite('decodedcv.png', img)
-   # plt.imshow(img)
-   # plt.colorbar()
-   # plt.savefig('colorbar.png')
-   # plt.close()
-   #return img
+   print('AF:',img.shape)
+   img = np.where((img == 1) | (img == 2), 255, 0) # !makes sure ctx stroke is `rgb(1,1,1)`
+   return img
+
+def preprocess_input(img):
+    img = cv2.resize(img, (64,64))
+    # todo: img = preprocess_input(img) (using built-in)
+
+def expected(img):
+    # todo: convert to (1,w,h,1)
+    # todo: get top 5 scores
+    return {
+        1: ['Q', 0.9],
+        2: ['B', 0.9],
+        3: ['S', 0.9],
+        4: ['M', 0.9],
+        5: ['Z', 0.9],
+    }
 
 # -----------------------------------------
 # CORS: List of servers to respond to...
@@ -74,9 +86,10 @@ app.add_middleware(
 
 
 # =============================================================================================================
+# =============================================================================================================
 # routes and related funcs
 # =============================================================================================================
-
+# =============================================================================================================
 @app.get("/wakeup")
 def get_initial_conditions(request: Request):
 	"""
@@ -85,7 +98,10 @@ def get_initial_conditions(request: Request):
 	return {'status': 'up'}
 
 
+
 @app.post("/predict")
 def predict(resp: someResponse):
-    readb64(resp.b64Image)
-    return {"label": 'rat'}
+    img = readb64(resp.b64Image)
+    # img = preprocess_input(img)
+    preds = expected(img)
+    return preds
